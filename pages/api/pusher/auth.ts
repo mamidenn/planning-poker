ee994f077e41b53352cc7acc50ed351a58f52235
Martin Dennhardt <martin@dennhardt.de>
2022-03-30 14:34:47 +0000

    fix badrequest

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -4,11 +4,14 @@ import Pusher from "pusher";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Pusher.AuthResponse | {}>
+  res: NextApiResponse<Pusher.AuthResponse>
 ) {
   const { socket_id, channel_name } = req.body;
   const user: Pusher.PresenceChannelData = JSON.parse(req.cookies.user);
-  if (!user.user_info!.name) res.status(403).json({});
+  if (!user.user_info!.name) {
+    res.status(403).end();
+    return;
+  }
   const auth = pusher.authenticate(socket_id, channel_name, user);
   res.status(200).json(auth);
 }
