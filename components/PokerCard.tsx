e4097300070b14ecce0c34b2c1ddb79e8ed3e554
Martin Dennhardt <martin@dennhardt.de>
2022-03-30 15:40:40 +0000

    add some flair to the cards

--- components/PokerCard.tsx
+++ components/PokerCard.tsx
@@ -13,14 +13,12 @@ const card: Record<CardState, (value: string) => JSX.Element> = {
     <div className="border-4 border-gray-50 outline outline-1 outline-gray-900/5 w-24 h-36 shadow-md rounded-lg bg-card-back" />
   ),
   faceUp: (value) => (
-    <div className="flex flex-col justify-between outline outline-1 outline-gray-900/5 w-24 h-36 p-1 shadow-md rounded-lg bg-white">
-      <span className="self-end underline underline-offset-2 text-sm">
-        {value}
-      </span>
-      <span className="self-center text-4xl font-semibold">{value}</span>
-      <span className="self-start underline underline-offset-2 text-sm rotate-180">
-        {value}
-      </span>
+    <div className="flex outline outline-1 outline-gray-900/5 w-24 h-36 p-1 shadow-md rounded-lg bg-white">
+      <div className="flex flex-col w-full justify-between bg-gradient-to-br from-slate-400 to-slate-700 bg-clip-text text-transparent">
+        <span className="self-start text-sm">{value}</span>
+        <span className="self-center text-4xl font-semibold">{value}</span>
+        <span className="self-end text-sm rotate-180">{value}</span>
+      </div>
     </div>
   ),
   unplayed: () => (
@@ -30,7 +28,7 @@ const card: Record<CardState, (value: string) => JSX.Element> = {
 
 export const PokerCard: FC<Props> = (props) => {
   return (
-    <div className="flex flex-col items-center space-y-1 text-gray-700">
+    <div className="flex flex-col items-center space-y-1">
       {card[props.state](props.value)}
       <span className="font-semibold">{props.player}</span>
     </div>
