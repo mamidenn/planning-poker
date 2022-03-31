3005441d07b97c10264d40eeeb572f3bf46eb80c
Martin Dennhardt <martin@dennhardt.de>
2022-03-31 10:45:01 +0000

    style the register page

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,14 @@ import React, { FC } from "react";
 
 interface Props {
   primary?: boolean;
+  type?: "button" | "submit" | "reset";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export const Button: FC<Props> = (props) => {
   return (
     <button
+      type={props.type ?? "button"}
       className={classNames(
         " py-2 px-4 shadow-md border-2 rounded-md text-white font-semibold active:translate-y-[1px] active:shadow-none",
         {
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,10 @@
+import { Button } from "components";
 import {
   GetServerSideProps,
   InferGetServerSidePropsType,
   NextPage,
 } from "next";
+import { useEffect, useRef } from "react";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   if (context.req.cookies.user)
@@ -23,12 +25,34 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const Register: NextPage = ({
   source,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const nameInput = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    nameInput.current?.focus();
+  });
+
   return (
-    <form method="post" action="/api/register">
-      <input type="text" name="name" />
-      <input type="hidden" name="source" value={source} />
-      <button type="submit">Let&apos;s go!</button>
-    </form>
+    <div className="flex justify-center items-center min-h-screen">
+      <form
+        autoComplete="off"
+        method="post"
+        action="/api/register"
+        className="flex flex-col gap-2 outline outline-1 outline-gray-900/5 rounded-lg shadow-lg p-8 bg-white"
+      >
+        <label htmlFor="name">Please choose a name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          ref={nameInput}
+          autoFocus
+          className="outline-none px-2 py-1 border-b-2 focus:border-b-orange-500"
+        />
+        <input type="hidden" name="source" value={source} />
+        <Button primary type="submit">
+          Let&apos;s go!
+        </Button>
+      </form>
+    </div>
   );
 };
 
