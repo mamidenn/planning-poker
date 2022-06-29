cc6c7000a1dd82a2a84576e713fd227a29551cb9
Martin Dennhardt <martin@dennhardt.de>
2022-06-29 13:13:38 +0200

    initial commit

---
+++ src/lib/Button.stories.ts
@@ -0,0 +1,11 @@
+import Button from "./Button.svelte"
+
+export default {
+    title: "Button",
+    component: Button
+}
+
+export const Default = () => ({
+    Component: Button,
+    props: {}
+})
