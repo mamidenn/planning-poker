3fb0287732124da90acc4b227a8f5f687e77d5ff
Martin Dennhardt <martin@dennhardt.de>
2022-07-02 13:28:19 +0200

    use grid for barchart

--- src/lib/Barchart.svelte
+++ src/lib/Barchart.svelte
@@ -5,19 +5,24 @@
 	$: maxAmount = Math.max(...Object.values(bars));
 </script>
 
-<div class="flex flex-row justify-center gap-2 h-44 relative mb-8">
-	{#each range(maxAmount + 1) as tick}
-		<hr class="absolute w-full" style="bottom: {(tick * 100) / maxAmount}%" />
-	{/each}
-	{#each Object.entries(bars) as [value, amount]}
-		<div class="flex flex-col items-center h-full">
-			<div class="h-full w-8 relative">
-				<div
-					class="w-full border-2 absolute bottom-0 left-0"
-					style="top: {100 - (amount * 100) / maxAmount}%"
-				/>
-			</div>
-			<p class="text-lg absolute -bottom-8">{value}</p>
-		</div>
+<div
+	class="grid gap-2 h-44 relative"
+	style="grid-template-rows: 1fr auto; grid-template-columns: repeat({Object.keys(bars)
+		.length},auto)"
+>
+	<div class="row-start-1 col-span-full relative">
+		{#each range(maxAmount + 1) as tick}
+			<div
+				class="absolute w-full ring-1 ring-purple-900/10"
+				style="bottom: {(tick * 100) / maxAmount}%"
+			/>
+		{/each}
+	</div>
+	{#each Object.entries(bars) as [value, amount], index}
+		<div
+			class="border-2 rounded-t-md border-purple-500 bg-gradient-to-br from-purple-500/75 to-cyan-500/75 row-start-1 self-end hover:shadow-xl transition-all z-10"
+			style="height: {(amount * 100) / maxAmount}%; grid-column-start: {index + 1}"
+		/>
+		<p class="text-lg font-semibold text-purple-500 row-start-2 text-center">{value}</p>
 	{/each}
 </div>
