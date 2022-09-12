421bd4af2d67f8f689c209397df5f3cd45745abc
Martin Dennhardt <martin@dennhardt.de>
2022-09-12 08:13:41 +0000

    fix svg id uniqueness

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import type { TransitionConfig } from 'svelte/transition';
+	import { uid } from 'uid';
 	type Orientation = 'faceUp' | 'faceDown';
 	export let label = 'C';
 	export let orientation: Orientation = 'faceUp';
 
-	const flip: (node: Element, params: any) => TransitionConfig = (_, { duration = 300 }) => ({
+	const id = uid();
+
+	const rotate: (node: Element, params: any) => TransitionConfig = (_, { duration = 300 }) => ({
 		duration,
 		css: (_, u) => `transform: rotateY(${Math.min(0.5, u) * 180}deg)`
 	});
@@ -12,14 +15,14 @@
 
 <div class="transition-enforcer">
 	{#if orientation === 'faceUp'}
-		<div class="card" in:flip out:flip>
+		<div class="card" in:rotate out:rotate>
 			<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
 				<defs>
-					<linearGradient id="g" gradientTransform="rotate(45)">
+					<linearGradient id={`g${id}`} gradientTransform="rotate(45)">
 						<stop stop-color="rgb(168 85 247)" />
 						<stop stop-color="rgb(6 182 212)" offset="1" />
 					</linearGradient>
-					<mask id="m">
+					<mask id={`m${id}`}>
 						<text
 							x="50%"
 							y="50%"
@@ -27,16 +30,16 @@
 							fill="white"
 							text-anchor="middle"
 							dominant-baseline="middle"
-							class="text-5xl font-semibold">{label}</text
+							class="label">{label}</text
 						>
 					</mask>
 				</defs>
-				<rect width="100%" height="100%" fill="url(#g)" mask="url(#m)" />
+				<rect width="100%" height="100%" fill={`url(#g${id})`} mask={`url(#m${id})`} />
 			</svg>
 		</div>
 	{:else}
-		<div class="card" in:flip out:flip>
-			<div class="h-full w-full rounded-sm bg-card-back-pattern" />
+		<div class="card" in:rotate out:rotate>
+			<div class="backface" />
 		</div>
 	{/if}
 </div>
@@ -51,5 +54,11 @@
 	}
 	.card {
 		@apply flex flex-col justify-center items-center w-24 h-36 border-2 border-purple-500 rounded-md shadow-md p-1;
+	}
+	.card .label {
+		@apply text-5xl font-semibold;
+	}
+	.card .backface {
+		@apply h-full w-full rounded-sm bg-card-back-pattern;
 	}
 </style>
