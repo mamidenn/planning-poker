3420f3cad89f4b1d7ec68a52a7ca749be681315a
Martin Dennhardt <martin@dennhardt.de>
2022-09-20 07:12:45 +0000

    add button component

---
+++ src/lib/Button.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+</script>
+
+<Meta title="Button" component={Button} argTypes={{}} />
+
+<Template let:args>
+	<Button {...args} />
+</Template>
+
+<Story name="Primary" args={{ label: 'Click me!' }} />
---
+++ src/lib/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let label: string;
+</script>
+
+<button on:click>{label}</button>
+
+<style>
+	button {
+		@apply text-purple-500 font-medium border-2 border-purple-500 rounded-md shadow-md active:shadow-none active:translate-y-[1px] px-4 py-2;
+	}
+</style>
