e61872a2cb136bf44ddf0054afc36a69e1f52938
Martin Dennhardt <martin@dennhardt.de>
2022-12-21 10:57:46 +0000

    add Heading story

---
+++ src/lib/Heading.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Heading from './Heading.svelte';
+</script>
+
+<Meta title="Heading" component={Heading} argTypes={{}} />
+
+<Template let:args>
+	<Heading {...args}>Heading</Heading>
+</Template>
+
+<Story name="Heading" />
