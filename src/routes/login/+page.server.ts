8c2bf57bd44fcec81e8abf293b7cbe4a03106bc5
Martin Dennhardt <martin@dennhardt.de>
2022-12-21 10:55:37 +0000

    fix sveltekit api change

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,4 +1,4 @@
-import { invalid, redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import { namePattern, user } from '$lib/user';
 
@@ -8,9 +8,8 @@ export const actions: Actions = {
 		const data = await request.formData();
 		const name = data.get('name')?.toString();
 
-		if (!name) return invalid(400, { name, validation: { name: 'Cannot be empty.' } });
-		if (!namePattern.test(name))
-			return invalid(400, { name, validation: { name: 'Invalid name.' } });
+		if (!name) return fail(400, { name, validation: { name: 'Cannot be empty.' } });
+		if (!namePattern.test(name)) return fail(400, { name, validation: { name: 'Invalid name.' } });
 		signup(name);
 		throw redirect(303, url.searchParams.get('url') || '/');
 	}
