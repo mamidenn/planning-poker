394032b74ddd17fcdbdf1e4a1e72ad15aa198b34
Martin Dennhardt <martin@dennhardt.de>
2024-09-25 09:34:30 +0000

    update supabase connection strings

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { env } from '$env/dynamic/public';
 
-export const supabase = createClient(
-	'https://rvdohsdtvhjtzgxsyffn.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2ZG9oc2R0dmhqdHpneHN5ZmZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM2MDIwNDcsImV4cCI6MTk3OTE3ODA0N30.Yp8giZBS4In5Sc9Ngqw9zRGqjYBz1MNAhg7cJKurcbo'
-);
+const supabaseUrl = env.PUBLIC_SUPABASE_URL;
+const supabaseKey = env.PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
